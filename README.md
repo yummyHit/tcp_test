b1b8d7a95ad13d2f36831ad87bb48492a5e13894
yummyHit <crazyboyi@naver.com>
2019-02-18 20:41:09 +0900

    refresh with epoll

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# tcp_test
+# 2016 data networking project
+It is only examine test project.
+Implemetation tcp functions; 3-way handshaking, flow control, congestion control
+
++ add to epoll test files
---
+++ epoll_test/errexit.c
@@ -0,0 +1,24 @@
+// va_list
+#include <stdarg.h>     
+#include <stdio.h>
+#include <stdlib.h>
+
+// Print error information
+int errexit(const char* format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	vfprintf(stderr, format, args);
+	va_end(args);
+	exit(1);
+}
+
+// Print work information
+void echo(const char* format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	vfprintf(stderr, format, args);
+	va_end(args);
+}
+
